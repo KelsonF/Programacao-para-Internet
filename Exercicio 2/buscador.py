4d9987d8d208dd8bd77ba795d201ea1dd4bf57d5
KelsonF <kfeduardo2003@gmail.com>
2023-03-02 02:26:40 -0300

    Atividade 2 --> Buscador

--- Exercicio 2/buscador.py
+++ Exercicio 2/buscador.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import re
 import requests_cache
 import requests
 
@@ -7,39 +8,22 @@ url = 'https://www.bbc.com/portuguese'
 requests_cache.install_cache('search_cache')
 
 
-def busca_termo(keyword, text):
-    for index in range(len(text)):
-        if text[index:index+len(keyword)] == keyword:
-            inicio = max(0, index)
-            fim = min(len(text), index+len(keyword) + 20)
-            contexto = text[inicio:fim]
-            print('-----------------------------------------------------------------------')
-            print(f"Trecho encontrado na página {url}: {contexto}")
-            print('-----------------------------------------------------------------------')
+def busca_termo(text):
+    for termo in text:
+        texto = termo.get_text()
+        if texto != '':
+            print('-------------------------------------')
+            print(f'Frase: {texto} --> Url: {url}')
+            print('-------------------------------------')
 
 def search(url, keyword, depth):
     response = requests.get(url)
     html = BeautifulSoup(response.text, 'html.parser')
-    texto = html.get_text().lower()
+    texto =  html.find_all(string=re.compile(keyword))
 
-    busca_termo(keyword, texto)
-
-    if depth > 0:
-        links = html.find_all('a')
-        for link in links:
-            try:
-                url_ = link.get('href')
-                if url != None:
-                    if url.startswith('http'):
-                        search(url_, keyword, depth-1)
-                    else:
-                        search(url+url_, keyword, depth-1)
-            except requests.exceptions.MissingSchema:
-                continue
-            except requests.exceptions.InvalidSchema:
-                continue
-    
+    busca_termo(texto)
     page_rank = {}
+
     links = html.find_all('a')
     for link in links:
         if 'href' in link.attrs:
@@ -49,10 +33,29 @@ def search(url, keyword, depth):
                     page_rank[next_url] += 1
                 else:
                     page_rank[next_url] = 1
+                    
+
+    while depth > 0:
+        links = html.find_all('a')
+        for link in links:
+            try:
+                if link not in page_rank:
+                    url_ = link.get('href')
+                    if url != None:
+                        if url.startswith('http'):
+                            search(url_, keyword, depth-1)
+                        else:
+                            search(url+url_, keyword, depth-1)
+            except requests.exceptions.MissingSchema:
+                continue
+            except requests.exceptions.InvalidSchema:
+                continue
+            
+        depth = depth-1
 
     sorted_pages = sorted(page_rank.items(), key=lambda x: x[1], reverse=True)
     print("Páginas ranqueadas:")
     for page, rank in sorted_pages:
         print(f"{page} - referências: {rank}")
      
-search(url, 'brasil', 0)
+search(url, 'brasil', 1)
